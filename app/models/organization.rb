16f8507fbc4c43fa823a63294c475b7107fb0587
Pratik <tispratik@gmail.com>
2010-04-25 18:31:04 -0500

    updates

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,4 +2,16 @@ class Organization < ActiveRecord::Base
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_user_id"
   has_one :mcontact
   has_many :teams
+  
+  def before_validation_on_create
+    self.permalink = Authlogic::Random.friendly_token
+  end
+  
+  def to_s
+    name
+  end
+  
+  def to_param
+    UrlStore.encode(permalink)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,33 @@
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+
+  config.active_record.colorize_logging = false
+  config.active_record.timestamped_migrations = false
   
   config.time_zone = 'UTC'
 
   config.gem 'haml'
   config.gem 'will_paginate'
-  config.gem 'formtastic'
 
+
+  config.gem 'email_veracity'
+  config.gem 'url_store'
+  config.gem 'delayed_job'
+  config.gem 'thinking-sphinx', :lib => 'thinking_sphinx'
+  config.gem 'ts-delayed-delta', :lib => false
+  config.gem 'searchlogic'
 end
+
+Delayed::Worker.backend = :active_record
+ActionView::Base.sanitized_allowed_tags.delete 'div'
+
+require 'paperclip'
+require 'thinking_sphinx/deltas/delayed_delta'
+
+UrlStore.defaults = {:secret => '55sfsasfjhjhj'}
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS[:default] = "%B %d, %Y %H:%M"
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS[:default] = '%Y-%m-%d'
