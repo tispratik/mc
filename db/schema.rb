57fed4cf3945b5ae0070d8a60b67bf969d7cd901
Pratik <tispratik@gmail.com>
2010-05-11 23:58:17 -0500

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100424215138) do
+ActiveRecord::Schema.define(:version => 100000000000000) do
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "mcontacts", :force => true do |t|
     t.string   "email"
